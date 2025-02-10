e8edecd45dfbbbb722df05680fea4eda481b6443
Farhan Mahmood N <81014687+MrLionByte@users.noreply.github.com>
2025-02-10 11:02:53 +0530

    Time: 8 ms (90.49%), Space: 17.9 MB (49.8%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,6 +1,6 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for i in ransomNote:
+        for i in set(ransomNote):
             if i not in magazine:
                 return False
             elif ransomNote.count(i) != magazine.count(i) and ransomNote.count(i) > magazine.count(i):
