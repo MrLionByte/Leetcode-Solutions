810a0f655836f497013f0f3bfdcac05923a48cc2
Farhan Mahmood N <81014687+MrLionByte@users.noreply.github.com>
2025-03-07 12:03:07 +0530

    Time: 0 ms (100%), Space: 17.6 MB (82.6%) - LeetHub

--- 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.py
+++ 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.py
@@ -1,13 +1,13 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        arr = []
         ans = [i for i in range(1,k+1)]
         count = 0
-        print("ANS",ans)
+        
         for i in range(len(nums)-1,-1,-1):
             count += 1
             if nums[i] in ans:
                 ans.remove(nums[i])
             if len(ans)== 0:
                 break
+
         return count
