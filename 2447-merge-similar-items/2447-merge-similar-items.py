f0382273d04526f39999823700e926f5cdf9bf28
Farhan Mahmood N <81014687+MrLionByte@users.noreply.github.com>
2025-05-10 11:09:07 +0530

    Time: 3 ms (84.06%), Space: 18.5 MB (81.16%) - LeetHub

--- 2447-merge-similar-items/2447-merge-similar-items.py
+++ 2447-merge-similar-items/2447-merge-similar-items.py
@@ -1,18 +1,21 @@
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        big_array = items1 if len(items1)>len(items2) else items2
-        small_array = items2 if big_array == items1 else items1 
+        count = {}
+        
+        for val, weight in items1:
+            if val in count:
+                count[val] += weight
+            else:
+                count[val] = weight
+        
+        for val, weight in items2:
+            if val in count:
+                count[val] += weight
+            else:
+                count[val] = weight
+        
+        result = []
+        for val in sorted(count.keys()):
+            result.append([val,count[val]])
 
-        for pair in small_array:
-            dummy = None
-            for main_pair in big_array:
-                if pair[0] == main_pair[0]:
-                    main_pair[1] += pair[1]
-                    dummy = None
-                    break
-                if pair[0] != main_pair[0]:
-                    dummy = pair
-            if dummy:
-                big_array.append(dummy)
-
-        return sorted(big_array)
+        return result
